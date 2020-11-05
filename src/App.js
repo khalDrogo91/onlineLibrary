24dab9118cb50e12efd576d51d7599045cd20131
Abhishek <abhishek11521@gmail.com>
2020-11-05 08:03:37 +0530

    Adding search functionality

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Book from './component/Book';
 import data from './data/book.json';
 
 
 function App() {
-  return <>
-  <div>
-    <h2>Book shell</h2>
-    
-    <input type ='text' placeholder='Search'></input>
-    <button type='submit'>search</button>
-    
-    <table style={{"borderWidth":"1px", 'borderColor':"#aaaaaa", 'borderStyle':'solid',
-     'border-width': 'medium'}}>
-      <tr>
-        <td>Id</td>
-        <td></td>
-        <td>Name</td>
-      </tr>
-      {data.map((book, index) =>
-        <tr> <Book key={index} {...book}></Book></tr>
-      )}
-    </table>
 
+  const [searchKey, setSearchkey] = useState("");
+
+  return (<>
+
+    <div>
+      <h2>Book shell</h2>
+
+      <input type='text'
+        placeholder='Search'
+        value={searchKey}
+        onChange={e => setSearchkey(e.target.value)}
+      />
+
+      <table style={{
+        "borderWidth": "1px", 'borderColor': "#aaaaaa", 'borderStyle': 'solid',
+        'border-width': 'medium'
+      }}>
+        
+        <tr>
+          <td>Id</td>
+          <td> </td>
+          <td>Name</td>
+        </tr>
+       {
+          data.filter(book => book.Title.includes(searchKey)).map((book, index) =>
+          <tr> <Book key={index} {...book}></Book></tr>
+        )}
+      </table>
 
 
 
     </div>
-  </>
+  </>)
 }
 
 export default App;
