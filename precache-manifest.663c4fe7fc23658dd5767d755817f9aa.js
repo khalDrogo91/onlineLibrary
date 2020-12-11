67bcb3f2c2028267dd160a283e341799383a7cb8
Abhishek <abhishek11521@gmail.com>
2020-12-12 00:17:40 +0530

    Updates

---
+++ precache-manifest.663c4fe7fc23658dd5767d755817f9aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fcc70f9baad99f32928",
+    "url": "/onlineLibrary/static/css/main.b8f22d1f.chunk.css"
+  },
+  {
+    "revision": "1fcc70f9baad99f32928",
+    "url": "/onlineLibrary/static/js/main.16ba624b.chunk.js"
+  },
+  {
+    "revision": "609d3965f66ece584308",
+    "url": "/onlineLibrary/static/js/runtime~main.5a94ad5d.js"
+  },
+  {
+    "revision": "ea80f15be06381829d33",
+    "url": "/onlineLibrary/static/js/2.bfcb2c56.chunk.js"
+  },
+  {
+    "revision": "70712c42f99ded67a1dc184324130bd7",
+    "url": "/onlineLibrary/index.html"
+  }
+];
